b638d757819089e535e8cf8e6069f1c88ffb25b6
Luca D'Angelo <info@lucadangelo.it>
2017-11-29 06:56:24 +0000

    split in subproject

---
+++ TEST001/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TEST001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TEST001();
+        }
+
+
+        public static void TEST001()
+        {
+            var cTokenSource = new CancellationTokenSource();
+            //LD any token created from this cancellation source will expire in 10000ms
+            cTokenSource.CancelAfter(10000);
+            //LD Create a cancellation token from CancellationTokenSource
+            var cToken = cTokenSource.Token;
+
+
+            // Create a task to run "GenerateNumbers" and pass the cancellation token
+            var t1 = Task<int>.Factory.StartNew(() => GenerateNumbers(cToken, " First call to: GenerateNumbers "), cToken);
+
+            // Create a task to run "GenerateNumbersNoToken" 
+            var t2 = Task<int>.Factory.StartNew(() => GenerateNumbersNoToken(" First call to: GenerateNumbers No Token "));
+
+            //LDregister a delegate for a callback when a cancellation request is made
+            cToken.Register(() => cancelNotification());
+            Console.ReadLine();
+
+        }
+
+        static int GenerateNumbers(CancellationToken ct, string str)
+        {
+            Console.WriteLine(str);
+            int i;
+            for (i = 0; i < 5000; i++)
+            {
+                Console.WriteLine("Generate Numbers, FOR id:{0}, then sleep 1500ms", i);
+                Thread.Sleep(1500);
+
+                //LD poll the IsCancellationRequested property to check if cancellation was requested 
+                if (ct.IsCancellationRequested)
+                { break; }
+            }
+            return i;
+        }
+
+        static int GenerateNumbersNoToken(string str)
+        {
+            Console.WriteLine(str);
+            int i;
+            for (i = 0; i < 20; i++)
+            {
+                Console.WriteLine("Generate Numbers No Token, FOR id:{0}/20, then sleep 1500ms", i);
+                Thread.Sleep(1500);
+            }
+            return i;
+        }
+
+        static void cancelNotification()
+        {
+            Console.WriteLine("Delegate Subscriber method - t1 stopped notification!");
+        }
+    }
+}
---
+++ TEST002/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TEST002
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            TEST002();
+        }
+
+        static void TEST002()
+        {
+            //LD nested threads managing using await. Any task will await the nested one to
+            // be completed.
+            EntryPoint();
+
+            //LD "NumberGenerator" run on the main thread
+            NumberGenerator();
+
+            Console.ReadLine();
+        }
+
+        static async Task EntryPoint()
+        {
+            Console.WriteLine("-> EntryPoint START'");
+            var task = CallASubTask();
+            await task;
+            Console.WriteLine("-> EntryPoint END " + " - " + task.Id);
+        }
+
+        static async Task CallASubTask()
+        {
+            Console.WriteLine("- - > CallASubTask START");
+            var task = Wait3SecAndReturnANumber();
+            int answer = await task;
+
+            Console.WriteLine("- - > CallASubTask END - answer: " + answer + " - TASK ID: " + task.Id);
+        }
+
+        static async Task<int> Wait3SecAndReturnANumber()
+        {
+            Console.WriteLine("- - - > Wait3SecAndReturnANumber");
+            var task = Task.Delay(3000);
+            await task;
+
+            int answer = 11 * 2;
+            Console.WriteLine("- - - > Wait3SecAndReturnANumber AFTER 3 SECS " + " - TASK ID: " + task.Id);
+            return answer;
+        }
+
+
+        static void NumberGenerator()
+        {
+            int i;
+            for (i = 0; i < 25; i++)
+            {
+                Console.WriteLine("Number: {0}", i);
+                Thread.Sleep(300);
+            }
+        }
+
+    }
+}
